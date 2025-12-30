8810983043677b40268a7fc374a6eade494b4c18
Atharvakumbhar1007 <143067366+Atharvakumbhar1007@users.noreply.github.com>
2025-12-30 18:18:37 +0530

    Initial commit

---
+++ src/day2/Userinput.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.util.Scanner;
+public class Userinput {
+
+	public static void main(String[] args) {
+		// Variable Declaration
+		int age; 
+		
+		// User Input
+		
+		// For user input we require Object of Scanner
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter your age: ");
+		age = input.nextInt();
+		
+		System.out.println("Your age = "+age);
+		
+		boolean isActive;
+		
+		System.out.println("Enter your status of active or not?");
+		isActive = input.nextBoolean();
+		
+		System.out.println("Active or not ?" +isActive);
+	}
+}
---
+++ src/day2/Variable.java
@@ -0,0 +1,47 @@
+package day2;
+
+public class Variable {
+	
+	public static void main(String[] args) {
+		
+		int age = 30;
+		System.out.println("Value of age = "+age);
+		
+		long num = 58_220_0000L;
+		System.out.println("Value of long number = "+num);
+		
+		char alp = 'H';
+		System.out.println("Value of alphabet = "+alp);
+		
+		float pi = 3.14F;
+		System.out.println("Value of decimal float = "+pi);
+		
+
+		boolean isPrime = true;
+		System.out.println("Prime status = "+isPrime);
+		
+	}
+}
+
+	// Variable: Named Container to store defined data
+			/*
+			 * PRIMITIVE DATA TYPE
+			 * 1.byte -> Whole  Number ->1byte = 8bits
+			 * 		  ->Range-2^7 to 0 to 2^7 -1
+			 * 		->-128 to 0 to 127
+			 * 2.short ->whole number -> 2byte = 16bits
+			 * 			->-2^15 to (2^15)-1
+			 * 3.int ->Whole Number-> 4 byte
+			 * 4.long ->Whole Number-> 8 byte
+			 * 5.float->Decimal Number -> 4 byte
+			 * 6.double->Decimal Number -> 8 byte
+			 * 7.char -> 2byte -> unicode encoding
+			 * 8.boolean -> True / False ->1 bit
+			 * 
+			 * 
+			 * NON PRIMITIVE DATA TYPE
+			 * 1.String
+			 * 2.Arrays
+			 * 3.Objects
+			 */
+		
