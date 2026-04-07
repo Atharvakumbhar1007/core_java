eb298bbff9e7a19b2450b587108c860f98694e58
Atharva <atharvakumbhar631@gmail.com>
2026-04-07 13:32:43 +0530

    static function and non static function

--- src/day11/Calculator.java
+++ src/day11/Calculator.java
@@ -1,22 +1,70 @@
 package day11;
 
-public class Calculator {
-	
-	//Methods
-	public void addition(int n1, int n2) {
-		System.out.println("Addition of "+n1+", "+n2+" = "+(n1+n2));
-	}
-	public void multiplication(int n1, int n2) {
-		System.out.println("Multiplication of "+n1+", "+n2+" = "+(n1*n2));
-	}
-	public void division(int n1, int n2) {
-		if(n2==0) {
-			System.out.println("Cannot divide by Zero");
-		}else {
-		System.out.println("division of "+n1+", "+n2+" = "+(n1/n2));
-		}
-		
-	 }
-}
-	
+import java.util.Scanner;
 
+public class Calculator {
+    
+	public static String brand = "Casio";
+	
+    // Methods
+    public static void addition(int n1, int n2) {
+        System.out.println("Addition of " + n1 + ", " + n2 + " = " + (n1 + n2));
+    }
+
+    public static void subtraction(int n1, int n2) {
+        System.out.println("Subtraction of " + n1 + ", " + n2 + " = " + (n1 - n2));
+    }
+
+    public static void multiplication(int n1, int n2) {
+        System.out.println("Multiplication of " + n1 + ", " + n2 + " = " + (n1 * n2));
+    }
+
+    public static void division(int n1, int n2) {
+        if (n2 == 0) {
+            System.out.println("Cannot divide by Zero");
+        } else {
+            System.out.println("Division of " + n1 + ", " + n2 + " = " + (n1 / n2));
+        }
+    }
+    
+    //non static method
+    
+    public void doCalculation(int n1,int n2, char 
+    		choice ) {
+    	switch (choice) {
+        case '+':
+            addition(n1, n2);
+            break;
+        case '-':
+            subtraction(n1, n2);
+            break;
+        case '*':
+            multiplication(n1, n2);
+            break;
+        case '/':
+            division(n1, n2);
+            break;
+        default:
+            System.out.println("Invalid Choice");
+    }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1, n2;
+
+        System.out.println("Enter number 1: ");
+        n1 = sc.nextInt();
+
+        System.out.println("Enter number 2: ");
+        n2 = sc.nextInt();
+
+        System.out.println("Select\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+
+        int choice = sc.nextInt();
+
+        Calculator calculator = new Calculator();
+
+    }
+}
--- src/day11/Casio.java
+++ src/day11/Casio.java
@@ -3,40 +3,41 @@ package day11;
 import java.util.Scanner;
 
 public class Casio {
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n1,n2;
+		int n1, n2;
 		System.out.println("Enter number 1: ");
-		n1=sc.nextInt();
+		n1 = sc.nextInt();
 		System.out.println("Enter number 2: ");
-		n2=sc.nextInt();
-		
-		System.out.println("Select\n1. Addition\n2. "
-				+ "Subtraction\n3. Mutiplication\n4.Division");
-		
+		n2 = sc.nextInt();
+
+		System.out.println("Select\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
 		int choice = sc.nextInt();
+
+		char operator = '0';
+		switch (choice) {
+		case 1:
+			operator = '+';
+			break;
+		case 2:
+			operator = '-';
+			break;
+		case 3:
+			operator = '*';
+			break;
+		case 4:
+			operator = '/';
+			break;
+		}
 		
 		Calculator calculator = new Calculator();
-		switch (choice) {
-		case 1: {
-			calculator.addition(n1, n2);
-			break;
-		}
-		case 2: {
-			calculator.subtraction(n1, n2);
-			break;
-		}
-		case 1: {
-			calculator.multiplication(n1, n2);
-			break;
-		}
-		case 1: {
-			calculator.division(n1, n2);
-			break;
-		}
-		default:
-			System.out.printl("Invalid Choice")
-		}
+		calculator.doCalculation(n1, n2, operator);
+		
+//		calculator.brand = "Natraj";
+		System.out.println("Brand = "+ calculator.brand);
+		System.out.println("Brand = "+ Calculator.brand);
+
 	}
+
 }
