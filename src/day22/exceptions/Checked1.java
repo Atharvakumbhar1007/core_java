2c9423d3afc8dc86477548cf0e6abb35f875856f
Atharva <atharvakumbhar631@gmail.com>
2026-06-18 13:38:09 +0530

    exceptions

--- src/day22/exceptions/Checked1.java
+++ src/day22/exceptions/Checked1.java
@@ -1,6 +1,7 @@
 package day22.exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Checked1 {
@@ -11,7 +12,24 @@ public class Checked1 {
 		File file = new File("demo.txt");
 		
 		System.out.println(file);
-		Scanner scanner = new Scanner(file); 
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			System.out.println("File Opened Successfully");
+		} catch (FileNotFoundException e) {
+			 // TODO: handle exception
+			System.out.println(e.getMessage());
+		}finally {
+			//Irrespective of try/catch execution,
+			//Finally will always execute
+			
+			System.out.println("Finally executed");
+			if(sc != null) {
+				sc.close();
+				System.out.println("File closed Successfully");
+			}
+		}
 	}
 
 }
