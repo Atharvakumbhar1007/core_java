22b2aea97ae39137bc2a2b0ddbf013a711c6a9ba
Atharva <atharvakumbhar631@gmail.com>
2026-07-07 12:52:37 +0530

    non_sync threading

--- src/day26/threading/part3/non_sync/Counter.java
+++ src/day26/threading/part3/non_sync/Counter.java
@@ -1,13 +1,12 @@
 package day26.threading.part3.non_sync;
 
 public class Counter {
-
-		 public int count = 0;
-		 
-		 
-		 public void increment() {
-			 count++;
-		 }
-		 
+	
+	public int count = 0;
+	
+	public void increment() {
+		count++;
+	}
 
 }
+
--- src/day26/threading/part3/non_sync/CustomThread.java
+++ src/day26/threading/part3/non_sync/CustomThread.java
@@ -1,19 +1,24 @@
 package day26.threading.part3.non_sync;
 
-public class CustomThread extends Thread{
+public class CustomThread extends Thread {
 	
 	public Counter counter;
-	
+
 	public CustomThread(Counter counter) {
+		
 		this.counter = counter;
 	}
-	 public void run() {
-		 
-		 for(int i=0; i<=100;i++) {
-			 counter.increment(); 
-		 }
-		 System.out.println("Value of counter by"
-		 +Thread.currentThread().getName()+" is = "+counter.count );
-	 }
 	
+	
+	@Override
+	public void run() {
+
+		for(int i = 1; i<=50;i++) {
+			counter.increment();
+			System.out.println("Value of counter by "+Thread.currentThread().getName()+" is = "+counter.count);
+		}
+		
+	}
+	
+
 }
