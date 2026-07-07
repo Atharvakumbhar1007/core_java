577a6ed514f9ccee5794a2c568de796bd3791ea2
Atharva <atharvakumbhar631@gmail.com>
2026-07-07 12:39:33 +0530

    threading

--- src/day26/threading/part3/non_sync/Counter.java
+++ src/day26/threading/part3/non_sync/Counter.java
@@ -2,9 +2,12 @@ package day26.threading.part3.non_sync;
 
 public class Counter {
 
-	 public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		 public int count = 0;
+		 
+		 
+		 public void increment() {
+			 count++;
+		 }
+		 
 
 }
